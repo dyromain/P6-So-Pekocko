1615686e800af8cc32bb219ca508ca35b8dd0bb0
Romain Dyrac <romain.dyrac@gmail.com>
2021-07-27 20:35:33 +0200

    cryptoJS

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,29 +7,54 @@ const jwt = require('jsonwebtoken');
 // Importation de l'utilisateur
 const User = require('../models/User');
 
+// Pour masquer l'adresse e-mail
+const CryptoJS = require("crypto-js");
+
+let hash = "&^(){@dhjdk$+£@#jkskdsab";
+
+// CryptoJS
+// const key = `${process.env.KEY}`;
+// const keyutf = CryptoJS.enc.Utf8.parse(key);
+// const iv = CryptoJS.enc.Base64.parse(key);
+
+// const encrypt = (string) => {
+//     const enc = CryptoJS.AES.encrypt(string, keyutf, { iv: iv });
+//     return enc.toString();
+// }
+
+
 // Inscription de l'utilisateur
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
+    const email = CryptoJS.HmacSHA256(req.body.email, `${hash}`).toString();    
+    const password = req.body.password;
+    bcrypt.hash(password, 10) // on sale 10 fois le password
       .then(hash => {
         const user = new User({
-          email: req.body.email,
+          email: email,
           password: hash
         });
+         
         user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
+          .then(() => { 
+              res.status(201).json({ message: 'Utilisateur créé !' })
+          }).catch(error => 
+             res.status(400).json({ error })
+          );
+      }).catch(error => res.status(500).json({ error }));
   };
+
 
 // Connexion de l'utilisateur
   exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    const email = CryptoJS.HmacSHA256(req.body.email, `${hash}`).toString();
+    const password = req.body.password;
+    User.findOne({email: email})
+    
       .then(user => {
         if (!user) {
           return res.status(401).json({ error: 'Utilisateur non trouvé !' });
         }
-        bcrypt.compare(req.body.password, user.password)
+        bcrypt.compare(password, user.password)
           .then(valid => {
             if (!valid) {
               return res.status(401).json({ error: 'Mot de passe incorrect !' });
